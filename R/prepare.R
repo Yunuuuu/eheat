bdfa4bb4f5ffb1e8aa373c5aa636d57665d3a2d3
yun <yunyunpp96@outlook.com>
2024-06-26 02:07:00 +0800

    fix Docs

--- R/prepare.R
+++ R/prepare.R
@@ -2,6 +2,10 @@
 #' @param object A [ExtendedHeatmap][eheat] or [ExtendedAnnotation][eanno]
 #' object.
 #' @param ... Additional arguments passed to specific methods
+#' @return An modified [ExtendedHeatmap][eheat] or [ExtendedAnnotation][eanno]
+#' object.
+#' @examples 
+#' eheat_prepare(eheat(matrix(rnorm(81), nrow = 9)))
 #' @export
 eheat_prepare <- function(object, ...) {
     UseMethod("eheat_prepare")
